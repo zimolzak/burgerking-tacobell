1c34933286fccbc8682c744962e8a14d28138053
Andy Zimolzak (2018 mbp) <andyzimolzak@gmail.com>
2021-01-05 09:56:45 -0600

    Text labels

--- menu-plot.R
+++ menu-plot.R
@@ -1,4 +1,4 @@
 library(ggplot2)
 library(here)
 X = read.csv(here('menus.csv'))
-ggplot(data=X, aes(x=kcal, y=price, color=restaurant)) + geom_point()
+ggplot(data=X, aes(x=kcal, y=price, color=restaurant, label=item)) + geom_point() + xlim(0,700) + ylim(0,5.5) + geom_text(vjust = 0, nudge_y = 0.1)
